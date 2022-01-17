2acb268205554db1c21ff18982fb3c637b70ea2e
MilanVulin <vulinmb@gmail.com>
2022-01-17 20:00:34 +0100

    Linked up authentication client to server

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import Cookies from "universal-cookie";
-import Axios from "axios";
+import axios from "axios";
 import signInImage from "../assets/signup.jpg";
+
+const cookies = new Cookies();
 
 const initialState = {
   fullName: "",
@@ -21,9 +23,28 @@ const Auth = () => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(form);
+
+    const {fullName, username, password, phoneNumber, avatarURL} = form;
+    const URL = "http://localhost:5000/auth";
+    const {data: {token, userId, hashedPassword}} = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
+      username,password, fullName, phoneNumber, avatarURL,
+    });
+
+    cookies.set('token', token);
+    cookies.set('username', username);
+    cookies.set('fullName', fullName);
+    cookies.set('userId', userId);
+
+    if(isSignup){
+    cookies.set('phoneNumber', phoneNumber);
+    cookies.set('avatarURL', avatarURL);
+    cookies.set('hashedPassword', hashedPassword);
+    }
+
+    window.location.reload();
+
   };
 
   const switchMode = () => {
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,9 @@
 const {connect} = require('getstream');
 const bcrypt = require('bcrypt');
-const StreamChat = require('stream-chat');
+const StreamChat = require('stream-chat').StreamChat;
 const crypto = require('crypto');
+
+require("dotenv").config();
 
 const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
@@ -26,13 +28,9 @@ const login = async (req, res) => {
         const {username, password} = req.body;
         const serverClient = connect(api_key, api_secret, app_id);
         const client = StreamChat.getInstance(api_key,api_secret);
-
         const {users} = await client.queryUsers({name: username});
-
         if(!users.length) return res.status(400).json({message: "User not found"});
-        
         const success = await bcrypt.compare(password, users[0].hashedPassword);
-        
         const token = serverClient.createUserToken(users[0].id);
 
         if(success){

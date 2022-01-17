7b5a8bd75865e8bb311ecacdbc6217b215b1e827
MilanVulin <vulinmb@gmail.com>
2022-01-17 19:14:03 +0100

    Added authentication and autorisation functionallity to the server side

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const CORS = require('cors');
+const express = require("express");
+const cors = require("cors");
+
+const authRoutes = require('./routes/auth.js');
 
 const app = express();
-const PORT = process.env || 5000;
+const PORT = process.env.PORT || 5000;
 
 require("dotenv").config();
 
@@ -10,9 +12,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get('/', (res, req) => {
-    res.send("Hello world");
+app.get("/", (req, res) => {
+  res.send("Hello world");
 });
 
-app.listen(PORT, () => 
-    console.log(`Server running on port ${PORT}!`));
+app.use('/auth', authRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}!`));

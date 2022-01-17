5b9677e0824dd03b47dc487fa62f72a42e794052
MilanVulin <vulinmb@gmail.com>
2022-01-17 13:21:24 +0100

    Added sidebar to the layout of the app

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,25 +1,32 @@
-import React from 'react'
-import { Channel, useChatContext } from 'stream-chat-react';
-import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
-import Cookies from 'universal-cookie';
-import HospitalIcon from '../assets/hospital/png';
+import React from "react";
+import { Channel, useChatContext } from "stream-chat-react";
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
+import Cookies from "universal-cookie";
+import HospitalIcon from "../assets/hospital.png";
+import LogoutIcon from "../assets/logout.png";
 
-const SideBar = () => {
-    <div className='channel-list__sidebar'>
-        <div className='channel-list__sidebar__icon1'>
-            <div className='icon1__inner'>
-                <img src='HospitalIcon' alt='Hospital' width='30'/>
-            </div>
-        </div>
+const SideBar = () => (
+  <div className="channel-list__sidebar">
+    <div className="channel-list__sidebar__icon1">
+      <div className="icon1__inner">
+        <img src={HospitalIcon} alt="Hospital" width="30" />
+      </div>
     </div>
-}
+
+    <div className="channel-list__sidebar__icon2">
+      <div className="icon1__inner">
+        <img src={LogoutIcon} alt="Logout" width="30" />
+      </div>
+    </div>
+  </div>
+);
 
 const ChannelListContainer = () => {
-    return (
-        <div>
-            <h1>ChannelListContainer</h1>
-        </div>
-    )
-}
+  return (
+    <>
+        <SideBar /> 
+    </>
+  );
+};
 
-export default ChannelListContainer
+export default ChannelListContainer;

08925288364f0c5e1c53a3870ab1a3dec8479e3d
MilanVulin <vulinmb@gmail.com>
2022-01-23 14:57:00 +0100

    Edit channel feature added and fixed full name display bug

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -26,25 +26,32 @@ const Auth = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const {fullName, username, password, phoneNumber, avatarURL} = form;
+    const { username, password, phoneNumber, avatarURL } = form;
+
     const URL = "http://localhost:5000/auth";
-    const {data: {token, userId, hashedPassword}} = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
-      username,password, fullName, phoneNumber, avatarURL,
+
+    const {
+      data: { token, userId, hashedPassword, fullName },
+    } = await axios.post(`${URL}/${isSignup ? "signup" : "login"}`, {
+      username,
+      password,
+      fullName: form.fullName,
+      phoneNumber,
+      avatarURL,
     });
 
-    cookies.set('token', token);
-    cookies.set('username', username);
-    cookies.set('fullName', fullName);
-    cookies.set('userId', userId);
+    cookies.set("token", token);
+    cookies.set("username", username);
+    cookies.set("fullName", fullName);
+    cookies.set("userId", userId);
 
-    if(isSignup){
-    cookies.set('phoneNumber', phoneNumber);
-    cookies.set('avatarURL', avatarURL);
-    cookies.set('hashedPassword', hashedPassword);
+    if (isSignup) {
+      cookies.set("phoneNumber", phoneNumber);
+      cookies.set("avatarURL", avatarURL);
+      cookies.set("hashedPassword", hashedPassword);
     }
 
     window.location.reload();
-
   };
 
   const switchMode = () => {
@@ -115,9 +122,7 @@ const Auth = () => {
               />
             </div>
             <div className="auth__form-container_fields-content_button">
-                <button className="">
-                    {isSignup ? "Sign Up" : "Sign in"}
-                </button>
+              <button className="">{isSignup ? "Sign Up" : "Sign in"}</button>
             </div>
           </form>
           <div className="auth__form-container_fields-account">
